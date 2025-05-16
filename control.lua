d8cfbc824e8d51198bec4b991347750d2e372b1b
RustyNova <rusty.nova.jsb@gmail.com>
2025-05-16 09:08:44 +0000

    added control script

--- control.lua
+++ control.lua
@@ -1,7 +1,38 @@
--- local lib = require("lib")
+local lib = require("lib")
 
--- script.on_event(defines.events.on_tick, function(event)
---     if game.tick % 60 == 0 then
---         game.print(lib.order("promethium-void-culture-dead"))
---     end
--- end)
+add_recipe_at_location_handler("firearm-magazine", "nauvis")
+
+
+---Initialize the controls for a recipe that can only be crafted at a specific space location
+---@param recipe any
+---@param space_location_name any
+function add_recipe_at_location_handler(recipe, space_location_name)
+    script.on_event(defines.events.on_space_platform_changed_state, function(event)
+        enable_recipe_at_location(event.platform, recipe, space_location_name)
+    end)
+end
+
+---Only enable a recipe if a space platform is at a specific space location
+---@param platform LuaSpacePlatform
+---@param recipe string
+---@param space_location_name string
+function enable_recipe_at_location(platform, recipe, space_location_name)
+    local disable = platform.space_location.name ~= space_location_name
+    toggle_recipe_on_surface(platform.surface, recipe, disable)
+end
+
+---Enable or disable a recipe on a whole surface
+---@param surface LuaSurface
+---@param recipe string
+---@param disable boolean
+function toggle_recipe_on_surface(surface, recipe, disable)
+    local entities = surface.find_entities_filtered({})
+
+    for _, entity in pairs(entities) do
+        if entity.get_recipe() == recipe then
+            local behaviour = entity.get_control_behavior()
+            ---@diagnostic disable-next-line: inject-field
+            behaviour.disable = disable
+        end
+    end
+end
