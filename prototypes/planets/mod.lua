be2b33c5cf42c85fceb49e90e4088cc761f31ba1
RustyNova <rusty.nova.jsb@gmail.com>
2025-05-16 13:14:41 +0000

    work

---
+++ prototypes/planets/mod.lua
@@ -0,0 +1,1 @@
+require("prototypes.planets.space-locations")
---
+++ prototypes/planets/space-locations.lua
@@ -0,0 +1,36 @@
+local lib = require("lib")
+local asteroid_util = require "__space-age__.prototypes.planet.asteroid-spawn-definitions"
+
+data:extend({
+    {
+        type = "space-location",
+        name = "black-hole",
+        icon = lib.icon("test"),
+        order = "g[black-hole]-b",
+        subgroup = "planets",
+        gravity_pull = 100,
+        distance = 60,
+        orientation = 0.22,
+        magnitude = 1.0,
+        label_orientation = 0.15,
+        asteroid_spawn_influence = 1,
+        draw_orbit = false,
+        asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.black_hole_asteroids, 0.9)
+    },
+    ---@type SpaceLocationPrototype
+    {
+        type = "space-location",
+        name = "black-hole-approach",
+        icon = lib.icon("test"),
+        order = "g[black-hole]-a",
+        subgroup = "planets",
+        gravity_pull = 10,
+        distance = 60,
+        orientation = 0.23,
+        magnitude = 1.0,
+        label_orientation = 0.15,
+        asteroid_spawn_influence = 1,
+        draw_orbit = false,
+        asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.black_hole_asteroids, 0.9)
+    },
+})
