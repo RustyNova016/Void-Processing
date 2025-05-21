86fd2a479d10626ea28b5f2af785cd519b8026a0
RustyNova <rusty.nova.jsb@gmail.com>
2025-05-21 15:28:48 +0000

    work

--- prototypes/technology/planets.lua
+++ prototypes/technology/planets.lua
@@ -8,12 +8,16 @@ data:extend({
         icon_size = 256,
         essential = false,
         effects = {
-            type = "unlock-space-location",
-            space_location = "black-hole-approach",
-            use_icon_overlay_constant = true
+            {
+                type = "unlock-space-location",
+                space_location = "black-hole-approach",
+                use_icon_overlay_constant = true
+            }
         },
         prerequisites = { "void-core-activation", "void-control-unit" },
-        unit = lib.technology.cost(500, lib.globals.technology.require_all_promethium, 60)
+        unit = lib.technology.cost(500, lib.globals.technology.require_all_promethium, 60),
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        planetslib_ensure_all_packs_from_vanilla_lab = true
     },
     {
         type = "technology",
@@ -22,11 +26,16 @@ data:extend({
         icon_size = 256,
         essential = false,
         effects = {
-            type = "unlock-space-location",
-            space_location = "black-hole",
-            use_icon_overlay_constant = true
+            {
+                type = "unlock-space-location",
+                space_location = "black-hole",
+                use_icon_overlay_constant = true
+            },
+            lib.technology.unlock_recipe("void-pylon-tuned")
         },
         prerequisites = { "void-core-activation", "void-control-unit" },
-        unit = lib.technology.cost(2500, lib.globals.technology.require_all_promethium, 60)
+        unit = lib.technology.cost(2500, lib.globals.technology.require_all_promethium, 60),
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        planetslib_ensure_all_packs_from_vanilla_lab = true
     },
 })
